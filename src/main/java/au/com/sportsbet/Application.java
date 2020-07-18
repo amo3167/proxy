b9080b9bc0a7d21e9e046d3de1b0364f7f839120
amo31 <amo3167@gmail.com>
2020-07-18 16:30:16 +1000

    Fix base package name. It caused controller not working

--- src/main/java/au/com/sportsbet/Application.java
+++ src/main/java/au/com/sportsbet/Application.java
@@ -3,8 +3,7 @@ package au.com.sportsbet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TODO: base package is wrong
-@SpringBootApplication(scanBasePackages = { "au.com.sportsbet.gps.proxy" })
+@SpringBootApplication(scanBasePackages = { "au.com.sportsbet.proxy" })
 public class Application {
 
     public static void main(final String[] args) {
